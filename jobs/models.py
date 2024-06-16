2874f33a284763842f60ee36b61caedcc9c919c8
Anthony Keith <spidey@linux.ht.home>
2024-06-16 03:18:38 -0400

    Added jobs logic for homepage
    Added the logic behind creating and displaying jobs. Also added links and small style edits to homepage.

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    thumbnail = models.ImageField(upload_to='images/')
+    job_name = models.CharField(default='Placeholder',max_length=75)
+    github_link = models.CharField(default='Placeholder', max_length=256)
     summary = models.CharField(max_length=256)
